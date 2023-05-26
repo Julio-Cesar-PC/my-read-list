28bbc23cb61b2c9ee79b8d13d9c260f0144e3aec
AndrizaCampanhol <amcampanhol@inf.ufsm.br>
2023-05-26 15:27:53 -0300

    modal changes

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -16,42 +16,47 @@ function BookList({ books }: any) {
 
   
   return (
-      <div className="flex flex-wrap gap-2 justify-center mt-5">
-        {books.map( (book: any) => (
-            <div key={book.id} onClick={() => handleBookClick(book)} className="transition ease-in-out delay-150 max-w-sm w-full lg:flex border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 hover:translate-y-1">
-            <div className="h-32 lg:h-48 lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
-                <img className="h-full" src={book.volumeInfo.imageLinks?.thumbnail || "book-placeholder.jpg" } alt="capa do livro" />
-            </div>
-            <div className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-              <div className="mb-8">
-                <div className="text-gray-900 font-bold text-xl mb-2">{book.volumeInfo.title}</div>
+    <div className="flex flex-wrap gap-2 justify-center mt-5">
+      {books.map((book: any) => (
+        <div key={book.id} onClick={() => handleBookClick(book)} className="transition ease-in-out delay-60 max-w-sm w-full lg:flex border border-gray-400 rounded hover:translate-y-1">
+          <div className="h-32 lg:h-52 lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+            <img className="h-full p-2" src={book.volumeInfo.imageLinks?.thumbnail || "book-placeholder.jpg"} alt="capa do livro" />
+          </div>
+          <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className="mb-8">
+              <div className="text-gray-900 font-bold text-xl mb-2">{book.volumeInfo.title}</div>
                 <p className="text-gray-700 text-base">Numero de páginas: {book.volumeInfo.pageCount}</p>
-                <p className="text-gray-700 text-base">Ano de publicação: {book.volumeInfo.publishedDate}</p>
+                <p className="text-gray-700 text-base">Data de publicação: {book.volumeInfo.publishedDate}</p>
               </div>
-              <div className="flex items-center">
-                <div className="text-sm">
-                  <p className="text-gray-900 leading-none">{book.volumeInfo.authors}</p>
-                  <p className="text-gray-600">{book.volumeInfo.publisher}</p>
-                </div>
+            <div className="flex items-center">
+              <div className="text-sm">
+                <p className="text-gray-900 leading-none">{book.volumeInfo.authors}</p>
+                <p className="text-gray-600">{book.volumeInfo.publisher}</p>
               </div>
             </div>
           </div>
-        )
-        )}
+        </div>
+      ))}
         <Modal show={isOpen} onClose={toggleModal}>
-        <div className="flex flex-col gap-2">
           <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-bold">{bookModal?.volumeInfo?.title}</h1>
-            <p className="text-lg">{bookModal?.volumeInfo?.authors}</p>
+            <div className="flex flex-col gap-2 p-2">
+              <h1 className="text-2xl font-bold">{bookModal?.volumeInfo?.title}</h1>
+              <p className="text-lg">{bookModal?.volumeInfo?.authors}</p>
+            </div>
+           <div className="flex flex-row gap-2">
+            <img className="w-1/3 h-1/3 p-2" src={bookModal?.volumeInfo?.imageLinks?.thumbnail || "book-placeholder.jpg"} alt="capa do livro" />
+            <div className="flex flex-col gap-2 w-2/3">
+            </div>
+            </div>
+           <div className="flex flex-row gap-2 p-3">
+              <p className="text-lg text-justify">{bookModal?.volumeInfo?.description}</p>
+            </div>
+            <div className="flex justify-center p-2">
+              <Button className="w-1/5 bg-gray-600 hover:bg-gray-400" onClick={toggleModal}>Fechar</Button>
+            </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <img src={bookModal?.volumeInfo?.imageLinks?.thumbnail || "book-placeholder.jpg" } alt="capa do livro" />
-            <p className="text-lg">{bookModal?.volumeInfo?.description}</p>
-          </div>
-        </div>
-        <Button onClick={toggleModal}>Fechar</Button>
-      </Modal>
-        </div>
+        </Modal>
+      </div>
     )
 }
 
--- pages/livros.tsx
+++ pages/livros.tsx
@@ -1,6 +1,5 @@
 import Layout from "../components/layout"
 import BookList from "../components/BookList"
-import { useSession } from "next-auth/react"
 import React, { useState } from "react"
 import axios from "axios"
 
@@ -21,8 +20,8 @@ export default function IndexPage() {
 
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <form className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center" onSubmit={handleSearch}>
+      <div className="flex flex-col items-center min-h-screen py-2 mt-2">
+        <form className="flex flex-col items-center w-full flex-1 px-20 text-center" onSubmit={handleSearch}>
           <label className="text-2xl">Pesquise por titulos</label>
           <div className="flex gap-2 w-full">
             <input id="search" className="border-gray-300 border-2 rounded-md w-full" placeholder="Pesquise por titulos ou autores..."></input>
