92f7d70c1a37e6d8839092709d571e982a44791e
AndrizaCampanhol <amcampanhol@inf.ufsm.br>
2023-05-29 19:26:01 -0300

    add my list component

--- pages/minha-lista.tsx
+++ pages/minha-lista.tsx
@@ -1,15 +1,42 @@
 import Layout from "../components/layout"
 import { useSession } from "next-auth/react"
+import MenuItems from "../components/menuItems"
+import { useState } from "react"
 
 export default function IndexPage() {
   const { data: session, status } = useSession()
+  const [books, setBooks] = useState([]);
 
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1>BEM VINDO!</h1>
-        <h2>{`Hello ${session?.user?.name}`}</h2>
+      <div className="flex flex-col  min-h-screen py-2"> 
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    Livro
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Nota
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Status
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Paginas
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    <span className="sr-only">Editar</span>
+                  </th>
+              </tr>
+            </thead>
+          <tbody>
+            <MenuItems books={books}/>
+          </tbody>
+        </table>
       </div>
+    </div>
     </Layout>
   )
 }
